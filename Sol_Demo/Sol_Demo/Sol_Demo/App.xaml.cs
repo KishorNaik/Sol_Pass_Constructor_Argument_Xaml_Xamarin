8491d695a3cb305944f258544daaf7937d065cf3
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-17 22:06:41 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Sol_Demo.Services;
+using Sol_Demo.ViewModels;
+using Sol_Demo.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sol_Demo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
